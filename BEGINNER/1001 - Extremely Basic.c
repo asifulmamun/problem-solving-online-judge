7b1bd4ef334a4b1a28133c1c89e97621bc8cf245
asifulmamun <asifulmamun@gmail.com>
2019-06-26 19:31:14 +0600

    Created 1001 - Extremely Basic
    Created.

---
+++ BEGINNER/1001 - Extremely Basic.c
@@ -0,0 +1,21 @@
+/*
+    1001 - Extremely Basic
+    Url: https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+*/
+
+#include <stdio.h>
+
+int main(){
+    // Declare
+    int A,B,X;
+
+    scanf("%d%d",&A,&B);
+
+    // Addisiton and asign
+    X=A+B;
+
+    // Print Result
+    printf("X = %d\n", X);
+
+return 0;
+}
